8c4cfcc1e74a1fef3b9a401de7a5ec9b35d3cefd
lim-bo <artemshap66@gmail.com>
2025-09-18 20:07:11 +0300

    feat: added Run api server method with graceful shutdown; added integrational test for login/register routes

--- internal/api/handlers_test.go
+++ internal/api/handlers_test.go
@@ -3,19 +3,32 @@ package api_test
 import (
 	"bytes"
 	"context"
+	"database/sql"
 	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/bytedance/sonic"
 	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 	"github.com/limbo/discipline/internal/api"
+	"github.com/limbo/discipline/internal/repository"
 	"github.com/limbo/discipline/internal/service"
 	"github.com/limbo/discipline/pkg/entity"
+	"github.com/pressly/goose"
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"golang.org/x/crypto/bcrypt"
 )
+
+func TestMain(m *testing.M) {
+	service.InitValidator()
+	m.Run()
+}
 
 type UserServiceMock struct {
 	success bool
@@ -153,4 +166,142 @@ func TestLogin(t *testing.T) {
 		serv.Login(rr, req)
 		assert.Equal(t, http.StatusInternalServerError, rr.Result().StatusCode)
 	})
+}
+
+func TestUsersHandlersIntegrational(t *testing.T) {
+	cfg := setupUsersTestDB(t)
+	repo := repository.NewUsersRepo(cfg)
+	userService := service.NewUserService(repo)
+	server := api.New(&api.ServicesList{
+		UserService: userService,
+	})
+	body, err := sonic.ConfigDefault.Marshal(api.RegisterRequest{
+		Name:     username,
+		Password: password,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var uid uuid.UUID
+	t.Run("successfully registered", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/auth/register", bytes.NewReader(body))
+		server.Register(rr, req)
+		assert.Equal(t, http.StatusCreated, rr.Result().StatusCode)
+		result := make(map[string]any)
+		err := sonic.ConfigDefault.NewDecoder(rr.Result().Body).Decode(&result)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer rr.Result().Body.Close()
+		uidStr, ok := result["uid"].(string)
+		if ok {
+			uid = uuid.MustParse(uidStr)
+		} else {
+			t.Error("invalid response body")
+		}
+	})
+	t.Run("error registered: invalid body", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/auth/register", nil)
+		server.Register(rr, req)
+		assert.Equal(t, http.StatusBadRequest, rr.Result().StatusCode)
+	})
+	t.Run("successfully logged in", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/auth/login", bytes.NewReader(body))
+		server.Login(rr, req)
+		assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
+		result := make(map[string]any)
+		err := sonic.ConfigDefault.NewDecoder(rr.Result().Body).Decode(&result)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer rr.Result().Body.Close()
+		uidStr, ok := result["uid"].(string)
+		var uidLogin uuid.UUID
+		if ok {
+			uidLogin = uuid.MustParse(uidStr)
+		} else {
+			t.Error("invalid response body")
+		}
+		assert.Equal(t, uid, uidLogin)
+	})
+	t.Run("error login: invalid body", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/auth/login", nil)
+		server.Register(rr, req)
+		assert.Equal(t, http.StatusBadRequest, rr.Result().StatusCode)
+	})
+	t.Run("error login: wrong password", func(t *testing.T) {
+		body, err := sonic.ConfigDefault.Marshal(api.RegisterRequest{
+			Name:     username,
+			Password: password + "12345",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/auth/login", bytes.NewReader(body))
+		server.Login(rr, req)
+		assert.Equal(t, http.StatusForbidden, rr.Result().StatusCode)
+	})
+	t.Run("error login: user not found", func(t *testing.T) {
+		body, err := sonic.ConfigDefault.Marshal(api.RegisterRequest{
+			Name:     username + "dasdwdasd",
+			Password: password,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/auth/login", bytes.NewReader(body))
+		server.Login(rr, req)
+		assert.Equal(t, http.StatusNotFound, rr.Result().StatusCode)
+	})
+}
+
+type testPGConfig struct {
+	connStr string
+}
+
+func (cfg *testPGConfig) ConnString() string {
+	return cfg.connStr
+}
+
+func setupUsersTestDB(t *testing.T) *testPGConfig {
+	container, err := postgres.Run(context.Background(), "postgres:17",
+		postgres.WithUsername("test_user"),
+		postgres.WithDatabase("barn"),
+		postgres.WithPassword("test_password"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(30*time.Second),
+		),
+	)
+	if err != nil {
+		t.Fatal("error running test container: " + err.Error())
+	}
+	connStr, err := container.ConnectionString(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	connStr += "sslmode=disable"
+	conn, err := sql.Open("postgres", connStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = goose.Up(conn, "../../migrations")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conn.Close()
+	t.Cleanup(func() {
+		container.Terminate(context.Background())
+	})
+	return &testPGConfig{
+		connStr: connStr,
+	}
 }
--- internal/api/server.go
+++ internal/api/server.go
@@ -1,12 +1,22 @@
 package api
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/limbo/discipline/internal/service"
+	"github.com/limbo/discipline/pkg/cleanup"
 )
 
 type Server struct {
 	mx          *chi.Mux
+	server      *http.Server
 	userService service.UserServiceI
 }
 
@@ -15,8 +25,50 @@ type ServicesList struct {
 }
 
 func New(servicesOptions *ServicesList) *Server {
+	mx := chi.NewMux()
 	return &Server{
-		mx:          chi.NewMux(),
+		mx: mx,
+		server: &http.Server{
+			Handler: mx,
+		},
 		userService: servicesOptions.UserService,
 	}
+}
+
+func (s *Server) mountEndpoint() {
+	s.mx.Route("/api/v1", func(r chi.Router) {
+		r.Route("/auth", func(r chi.Router) {
+			r.Post("/register", s.Register)
+			r.Post("/login", s.Login)
+		})
+	})
+}
+
+func (s *Server) Run(address string) error {
+	s.mountEndpoint()
+	s.server.Addr = address
+	go func() {
+		log.Printf("Server starting on %s", address)
+		if err := s.server.ListenAndServe(); err != nil {
+			log.Fatal("Server failed: " + err.Error())
+		}
+	}()
+	return s.waitForShutdown()
+}
+
+func (s *Server) waitForShutdown() error {
+	closeCh := make(chan os.Signal, 1)
+	signal.Notify(closeCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	<-closeCh
+	log.Println("Shutting down server...")
+	cleanup.CleanUp()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+	if err := s.server.Shutdown(ctx); err != nil {
+		log.Printf("Server failed to shutdown: %v", err)
+		return err
+	}
+	log.Println("Server stopped")
+	return nil
 }
