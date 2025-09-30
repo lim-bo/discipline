b78f878ab3e1e93529cf29102f43ce4ccd0824ce
lim-bo <artemshap66@gmail.com>
2025-09-30 20:23:48 +0300

    feat: added tests for habits handlers

--- internal/api/handlers_test.go
+++ internal/api/handlers_test.go
@@ -5,21 +5,28 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/bytedance/sonic"
+	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 	"github.com/limbo/discipline/internal/api"
+	errorvalues "github.com/limbo/discipline/internal/error_values"
 	"github.com/limbo/discipline/internal/repository"
 	"github.com/limbo/discipline/internal/service"
+	"github.com/limbo/discipline/internal/service/mocks"
 	"github.com/limbo/discipline/pkg/entity"
 	jwtservice "github.com/limbo/discipline/pkg/jwt_service"
 	"github.com/pressly/goose"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -327,6 +334,225 @@ func TestUsersHandlersIntegrational(t *testing.T) {
 		server.Login(rr, req)
 		assert.Equal(t, http.StatusNotFound, rr.Result().StatusCode)
 	})
+}
+
+var (
+	userID = uuid.New()
+)
+
+func TestCreateHabit(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	hService := mocks.NewMockHabitsServiceI(ctrl)
+	serv := api.New(&api.ServicesList{
+		HabitsService: hService,
+	})
+	habit := api.CreateHabitRequest{
+		Title:       "test_habit",
+		Description: "test_habit_description",
+	}
+	body, err := sonic.ConfigDefault.Marshal(habit)
+	require.NoError(t, err)
+	habitID := uuid.New()
+
+	testCases := []struct {
+		ExpectedCode int
+		MockPrepFunc func()
+		Body         io.Reader
+	}{
+		{
+			ExpectedCode: http.StatusCreated,
+			MockPrepFunc: func() {
+				hService.EXPECT().CreateHabit(gomock.Any(), userID, service.CreateHabitRequest{
+					Title:       habit.Title,
+					Description: habit.Description,
+				}).Return(&entity.Habit{
+					ID:          habitID,
+					UserID:      uid,
+					Title:       habit.Title,
+					Description: habit.Description,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				}, nil)
+			},
+			Body: bytes.NewReader(body),
+		},
+		{
+			ExpectedCode: http.StatusConflict,
+			MockPrepFunc: func() {
+				hService.EXPECT().CreateHabit(gomock.Any(), userID, service.CreateHabitRequest{
+					Title:       habit.Title,
+					Description: habit.Description,
+				}).Return(nil, errorvalues.ErrUserHasHabit)
+			},
+			Body: bytes.NewReader(body),
+		},
+		{
+			ExpectedCode: http.StatusNotFound,
+			MockPrepFunc: func() {
+				hService.EXPECT().CreateHabit(gomock.Any(), userID, service.CreateHabitRequest{
+					Title:       habit.Title,
+					Description: habit.Description,
+				}).Return(nil, errorvalues.ErrUserNotFound)
+			},
+			Body: bytes.NewReader(body),
+		},
+		{
+			ExpectedCode: http.StatusInternalServerError,
+			MockPrepFunc: func() {
+				hService.EXPECT().CreateHabit(gomock.Any(), userID, service.CreateHabitRequest{
+					Title:       habit.Title,
+					Description: habit.Description,
+				}).Return(nil, errors.New("service error"))
+			},
+			Body: bytes.NewReader(body),
+		},
+		{
+			ExpectedCode: http.StatusBadRequest,
+			MockPrepFunc: func() {},
+			Body:         bytes.NewReader([]byte("corrupted")),
+		},
+	}
+	for _, tc := range testCases {
+		tc.MockPrepFunc()
+		rr := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodPost, "/api/habits", tc.Body)
+		r = r.WithContext(context.WithValue(r.Context(), "User-ID", userID))
+		serv.CreateHabit(rr, r)
+		assert.Equal(t, tc.ExpectedCode, rr.Result().StatusCode)
+		if tc.ExpectedCode == http.StatusCreated {
+			resp, _ := io.ReadAll(rr.Result().Body)
+			fmt.Println(string(resp))
+		}
+	}
+}
+func TestGetHabits(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	hService := mocks.NewMockHabitsServiceI(ctrl)
+	serv := api.New(&api.ServicesList{
+		HabitsService: hService,
+	})
+	habits := make([]*entity.Habit, 0, 10)
+	for i := range 10 {
+		habits = append(habits, &entity.Habit{
+			ID:          uuid.New(),
+			UserID:      userID,
+			Title:       fmt.Sprintf("test_habit_%d", i+1),
+			Description: "blah blah blah",
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+		})
+	}
+	testCases := []struct {
+		ExpectedCode        int
+		MockPrepFunc        func()
+		Limit               int
+		Page                int
+		ExpectedHabitsCount int
+	}{
+		{
+			ExpectedCode: http.StatusOK,
+			MockPrepFunc: func() {
+				hService.EXPECT().GetUserHabits(gomock.Any(), userID, service.PaginationOpts{
+					Limit:  10,
+					Offset: 0,
+				}).Return(habits, nil)
+			},
+			Page:                1,
+			Limit:               10,
+			ExpectedHabitsCount: 10,
+		},
+		{
+			ExpectedCode: http.StatusOK,
+			MockPrepFunc: func() {
+				hService.EXPECT().GetUserHabits(gomock.Any(), userID, service.PaginationOpts{
+					Limit:  4,
+					Offset: 4,
+				}).Return(habits[2:6], nil)
+			},
+			Page:                2,
+			Limit:               4,
+			ExpectedHabitsCount: 4,
+		},
+		{
+			ExpectedCode: http.StatusInternalServerError,
+			MockPrepFunc: func() {
+				hService.EXPECT().GetUserHabits(gomock.Any(), userID, service.PaginationOpts{
+					Limit:  10,
+					Offset: 0,
+				}).Return(nil, errors.New("service error"))
+			},
+			Page:                1,
+			Limit:               10,
+			ExpectedHabitsCount: 0,
+		},
+	}
+	for _, tc := range testCases {
+		tc.MockPrepFunc()
+		rr := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/api/habits", nil)
+		q := r.URL.Query()
+		q.Add("limit", strconv.Itoa(tc.Limit))
+		q.Add("page", strconv.Itoa(tc.Page))
+		r.URL.RawQuery = q.Encode()
+		r = r.WithContext(context.WithValue(r.Context(), "User-ID", userID))
+		serv.GetHabits(rr, r)
+		assert.Equal(t, tc.ExpectedCode, rr.Result().StatusCode)
+		if rr.Result().StatusCode == http.StatusOK {
+			var resp api.GetHabitsResponse
+			err := sonic.ConfigDefault.NewDecoder(rr.Body).Decode(&resp)
+			require.NoError(t, err)
+			assert.Equal(t, tc.ExpectedHabitsCount, len(resp.Habits))
+		}
+	}
+}
+func TestDeleteHabit(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	hService := mocks.NewMockHabitsServiceI(ctrl)
+	serv := api.New(&api.ServicesList{
+		HabitsService: hService,
+	})
+	habitID := uuid.New()
+	testCases := []struct {
+		ExpectedCode int
+		MockPrepFunc func()
+	}{
+		{
+			ExpectedCode: http.StatusOK,
+			MockPrepFunc: func() {
+				hService.EXPECT().DeleteHabit(gomock.Any(), habitID, userID).Return(nil)
+			},
+		},
+		{
+			ExpectedCode: http.StatusNotFound,
+			MockPrepFunc: func() {
+				hService.EXPECT().DeleteHabit(gomock.Any(), habitID, userID).Return(errorvalues.ErrHabitNotFound)
+			},
+		},
+		{
+			ExpectedCode: http.StatusNotFound,
+			MockPrepFunc: func() {
+				hService.EXPECT().DeleteHabit(gomock.Any(), habitID, userID).Return(errorvalues.ErrWrongOwner)
+			},
+		},
+		{
+			ExpectedCode: http.StatusInternalServerError,
+			MockPrepFunc: func() {
+				hService.EXPECT().DeleteHabit(gomock.Any(), habitID, userID).Return(errors.New("service error"))
+			},
+		},
+	}
+	for _, tc := range testCases {
+		tc.MockPrepFunc()
+		rr := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodDelete, "/api/habits/"+habitID.String(), nil)
+		r = r.WithContext(context.WithValue(r.Context(), "User-ID", userID))
+		r.SetPathValue("id", habitID.String())
+		serv.DeleteHabit(rr, r)
+		assert.Equal(t, tc.ExpectedCode, rr.Result().StatusCode)
+	}
+}
+func TestHabitsCRUDIntegrational(t *testing.T) {
+
 }
 
 type testPGConfig struct {
