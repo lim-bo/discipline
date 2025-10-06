7ff077c9a39e6ed3ee310931e495a2f234eb332b
lim-bo <artemshap66@gmail.com>
2025-10-06 20:23:00 +0300

    feat: api wrapped into docker container

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,8 +5,13 @@
 package main
 
 import (
+	"log"
+
+	"github.com/limbo/discipline/internal/api"
+	"github.com/limbo/discipline/internal/repository"
 	"github.com/limbo/discipline/internal/service"
 	"github.com/limbo/discipline/pkg/config"
+	jwtservice "github.com/limbo/discipline/pkg/jwt_service"
 )
 
 func init() {
@@ -15,4 +20,21 @@ func init() {
 
 func main() {
 	cfg := config.New()
+	dbCfg := repository.PGCfg{
+		Address:  cfg.GetString("POSTGRES_DB_ADDRESS"),
+		Username: cfg.GetString("POSTGRES_USER"),
+		Password: cfg.GetString("POSTGRES_PASSWORD"),
+		DB:       cfg.GetString("POSTGRES_DB"),
+	}
+	userService := service.NewUserService(repository.NewUsersRepo(&dbCfg))
+	habitService := service.NewHabitsService(repository.NewHabitsRepo(&dbCfg))
+	serv := api.New(&api.ServicesList{
+		UserService:   userService,
+		HabitsService: habitService,
+		JwtService:    jwtservice.New(cfg.GetString("JWT_SECRET")),
+	})
+	err := serv.Run(cfg.GetString("API_ADDRESS"))
+	if err != nil {
+		log.Println("Server error: " + err.Error())
+	}
 }
